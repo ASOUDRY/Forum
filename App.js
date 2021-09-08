b0798a00146b76cd38917403b3be81f3eb2db056
Alexander Soudry <soudryalex@gmail.com>
2021-09-08 00:03:04 -0400

    attempted to add Jsdoc documentation

--- App.js
+++ App.js
@@ -11,6 +11,15 @@ import { createStackNavigator } from '@react-navigation/stack';
 export default function App() {
   const Stack = createStackNavigator();
   return (
+    /**
+     * Navigates between the two imported screens
+     * (NavigationContainer} This container moniters the app state change
+     * Stack.Navigator This Navigator transitions between the two screens dictated in Stack.Screen
+     * initalRouteName  The name of the route to render on first load of the navigator
+     * @Method Stack.Screen
+     * @prop name The name of the screan
+     * @prop compoment This takes the imported component. 
+     */
     <NavigationContainer>
    <Stack.Navigator initialRouteName="Start" >
         <Stack.Screen
